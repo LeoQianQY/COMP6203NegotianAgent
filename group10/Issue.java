99973818c7b5f29803f67390d8d3c609631021db
Leo <526625275@qq.com>
2021-11-20 17:22:16 +0000

    first time commit

---
+++ group10/Issue.java
@@ -0,0 +1,6 @@
+package group10;
+
+public class Issue {
+    int a = 10;
+
+}
---
+++ group10/MyAgent.java
@@ -0,0 +1,148 @@
+package group10;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import genius.core.AgentID;
+import genius.core.Bid;
+import genius.core.actions.Accept;
+import genius.core.actions.Action;
+import genius.core.actions.EndNegotiation;
+import genius.core.actions.Offer;
+import genius.core.issue.Issue;
+import genius.core.issue.IssueDiscrete;
+import genius.core.issue.ValueDiscrete;
+import genius.core.parties.AbstractNegotiationParty;
+import genius.core.parties.NegotiationInfo;
+import genius.core.utility.AbstractUtilitySpace;
+import genius.core.utility.AdditiveUtilitySpace;
+import genius.core.utility.EvaluatorDiscrete;
+
+/**
+ * A simple example agent that makes random bids above a minimum target utility. 
+ *
+ * @author Tim Baarslag
+ */
+public class MyAgent extends AbstractNegotiationParty
+{
+	private static double MINIMUM_TARGET = 0.8;
+	private Bid lastOffer;
+	private int[][] table = new int[3][3];
+
+
+	static Comparator<Integer> cmp = new Comparator<Integer>() {
+		public int compare(Integer e1, Integer e2) {
+			return e2 - e1;
+		}
+	};
+
+
+	/**
+	 * Initializes a new instance of the agent.
+	 */
+	@Override
+	public void init(NegotiationInfo info) 
+	{
+		super.init(info);
+		System.out.println("init");
+		AbstractUtilitySpace utilitySpace = info.getUtilitySpace();
+		AdditiveUtilitySpace additiveUtilitySpace = (AdditiveUtilitySpace) utilitySpace;
+
+		List<Issue> issues = additiveUtilitySpace.getDomain().getIssues();
+
+		for (Issue issue : issues) {
+			int issueNumber = issue.getNumber();
+			System.out.println(issueNumber);
+			System.out.println(">> " + issue.getName() + " weight: " + additiveUtilitySpace.getWeight(issueNumber));
+
+			// Assuming that issues are discrete only
+			IssueDiscrete issueDiscrete = (IssueDiscrete) issue;
+			EvaluatorDiscrete evaluatorDiscrete = (EvaluatorDiscrete) additiveUtilitySpace.getEvaluator(issueNumber);
+
+			for (ValueDiscrete valueDiscrete : issueDiscrete.getValues()) {
+				System.out.println(valueDiscrete.getValue());
+				System.out.println("Evaluation(getValue): " + evaluatorDiscrete.getValue(valueDiscrete));
+				try {
+					System.out.println("Evaluation(getEvaluation): " + evaluatorDiscrete.getEvaluation(valueDiscrete));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Makes a random offer above the minimum utility target
+	 * Accepts everything above the reservation value at the end of the negotiation; or breaks off otherwise. 
+	 */
+	@Override
+	public Action chooseAction(List<Class<? extends Action>> possibleActions) {
+		// Check for acceptance if we have received an offer
+		if (lastOffer != null) {
+			System.out.println(timeline.getTime());
+			if (timeline.getTime() >= 0.9)
+				if (getUtility(lastOffer) >= utilitySpace.getReservationValue()) {
+					System.out.println("==================Accept");
+					return new Accept(getPartyId(), lastOffer);
+				} else {
+					System.out.println("==================End");
+					return new EndNegotiation(getPartyId());
+				}
+		}
+		// Otherwise, send out a random offer above the target utility 
+		return new Offer(getPartyId(), generateRandomBidAboveTarget());
+	}
+
+	private Bid getMaxUtilityBid() {
+		try {
+			return utilitySpace.getMaxUtilityBid();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private Bid generateRandomBidAboveTarget() 
+	{
+		Bid randomBid;
+		double util;
+		int i = 0;
+		// try 100 times to find a bid under the target utility
+		do 
+		{
+			randomBid = generateRandomBid();
+			util = utilitySpace.getUtility(randomBid);
+		} 
+		while (util < MINIMUM_TARGET && i++ < 100);		
+		return randomBid;
+	}
+
+	/**
+	 * Remembers the offers received by the opponent.
+	 */
+	@Override
+	public void receiveMessage(AgentID sender, Action action) 
+	{
+		if (action instanceof Offer) 
+		{
+			lastOffer = ((Offer) action).getBid();
+		}
+	}
+
+	@Override
+	public String getDescription() 
+	{
+		return "hahahahhahahahah";
+	}
+
+	/**
+	 * This stub can be expanded to deal with preference uncertainty in a more sophisticated way than the default behavior.
+	 */
+	@Override
+	public AbstractUtilitySpace estimateUtilitySpace() 
+	{
+		return super.estimateUtilitySpace();
+	}
+
+}
